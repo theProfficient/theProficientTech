a8f8083f1fbe5d801c817946a6220cd2f4823009
theProficient <theproficienttech333@gmail.com>
2023-03-07 16:41:04 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-/** @format */
-
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const route = require("./route/routes");
+
+
+app.use("/", route);
 //middleware
 app.use(express.json());
-
-const route = require("./route/routes");
-app.use("/", route);
+app.use(cors());
+app.options('*', cors())
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -24,6 +26,10 @@ mongoose
     console.log("no connected");
   });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express app running on port" + (process.env.port || 3000));
+// app.listen(process.env.PORT || 5000, function () {
+//   console.log("Express app running on port" + (process.env.port || 5000));
+// });
+
+app.listen(5000, '0.0.0.0', function() {
+  console.log('Listening to port:  ' + 5000);
 });
