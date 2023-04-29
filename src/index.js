7dc7dedd17dbb968786b5b88257c9123a1acc20d
theProficient <theproficienttech333@gmail.com>
2023-04-29 14:47:20 +0530

    using node-cron

--- src/index.js
+++ src/index.js
@@ -11,41 +11,41 @@ mongoose.set("strictQuery", false);
 
 //____________________for development____________________________________
 
-mongoose
-  .connect(
-    "mongodb+srv://theproficienttech333:gzYGYI5pD4oAUvim@cluster0.gp7jlnb.mongodb.net/game",
-    {
-      useNewUrlParser: true,
-    }
-  )
-  .then(() => {
-    console.log("MongoDB is connected for development");
-  })
-  .catch((error) => {
-    console.log("Not connected");
-  });
-app.listen(process.env.PORT || 8000, function () {
-  console.log("Express app running on port" + " " +(process.env.port || 8000));
-});
-
-  //_________________________for local________________________
-
-//   mongoose
+// mongoose
 //   .connect(
-//     "mongodb+srv://nikita1:7CSKh9nBmgBm27YC@cluster0.suzof1p.mongodb.net/nikita",
+//     "mongodb+srv://theproficienttech333:gzYGYI5pD4oAUvim@cluster0.gp7jlnb.mongodb.net/game",
 //     {
 //       useNewUrlParser: true,
 //     }
 //   )
 //   .then(() => {
-//     console.log("MongoDB is connected for local");
+//     console.log("MongoDB is connected for development");
 //   })
 //   .catch((error) => {
 //     console.log("Not connected");
 //   });
-
-
-// app.listen(process.env.PORT || 4000, function () {
-//   console.log("Express app running on port" + " " +(process.env.port || 4000));
+// app.listen(process.env.PORT || 8000, function () {
+//   console.log("Express app running on port" + " " +(process.env.port || 8000));
 // });
+
+  //_________________________for local________________________
+
+  mongoose
+  .connect(
+    "mongodb+srv://nikita1:7CSKh9nBmgBm27YC@cluster0.suzof1p.mongodb.net/nikita",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB is connected for local");
+  })
+  .catch((error) => {
+    console.log("Not connected");
+  });
+
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log("Express app running on port" + " " +(process.env.port || 4000));
+});
 
